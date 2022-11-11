c42ab3de6119d64b5903eb3b5fcf303ea0ff4b05
OU-DEVELOPERS <115209504+ou-developers@users.noreply.github.com>
2022-11-11 23:35:44 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ resource "oci_core_virtual_network" "vcn" {
   compartment_id = var.compartment_ocid
   cidr_block     = var.vcn_cidr
   dns_label      = var.vcn_dns_label
-  display_name   = "IAD-DOP-LAB05-1-VCN-01"
+  display_name   = var.vcn_dns_label
 }
 
 # Internet Gateway
@@ -29,7 +29,7 @@ resource "oci_core_subnet" "subnet" {
   compartment_id      = var.compartment_ocid
   vcn_id              = oci_core_virtual_network.vcn.id
   cidr_block          = cidrsubnet(var.vcn_cidr, 8, 1)
-  display_name        = "Subnet-IAD-DOP-LAB05-1-VCN-01"
+  display_name        = var.dns_label
   dns_label           = var.dns_label
   route_table_id      = oci_core_route_table.PublicRT.id
   security_list_ids   = [oci_core_security_list.securitylist.id]
